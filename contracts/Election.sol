88f88518956a6c0b5c2d7c05bdfbaaeae1432b4b
huydum22 <45447106+huydum22@users.noreply.github.com>
2020-06-22 14:36:00 +0700

    config add vote event in sodility file

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,27 +1,52 @@
 pragma solidity >=0.4.21 <0.7.0;
 
 contract Election {
-    // Model candidate
+
+// Model a Candidate
     struct Candidate {
-        uint256 id;
+        uint id;
         string name;
-        uint256 voteCount;
+        uint voteCount;
     }
-    // Read/write Candidates
-    mapping(uint256 => Candidate) public candidates;
+    event votedEvent (
+        uint indexed _candidateId
+    );
+
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
+
+    // Read/write candidates
+    mapping(uint => Candidate) public candidates;
 
     // Store Candidates Count
-    uint256 public candidatesCount;
+    uint public candidatesCount;
 
-    // Add candidate
-    function addCandidate(string memory _name) private {
-        candidatesCount++;
+    // Constructor
+    constructor () public {
+        addCandidate("Barack Obama");
+        addCandidate("Bill Clinton");
+    }
+function addCandidate (string memory  _name) private {
+        candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 
-    // Constructor
-    constructor() public {
-        addCandidate("Candidate 1");
-        addCandidate("Candidate 2");
-    }
+function vote ( uint _candidateId) public {
+    // require that they haven't voted before
+    require(!voters[msg.sender]);
+
+    // require a valid candidate
+    require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+    // record that voter has voted
+    voters[msg.sender] = true;
+
+    // update candidate vote Count
+    candidates[_candidateId].voteCount ++;
+
+    // trigger voted event
+    emit  votedEvent(_candidateId);
+}
+
+
 }
